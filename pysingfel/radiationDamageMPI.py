7f2776ccdab30f5c617883a5a654fd6c59309522
Haoyuan Li <hyli16@stanford.edu>
2018-08-29 17:36:59 +0800

    minor

--- pysingfel/radiationDamageMPI.py
+++ pysingfel/radiationDamageMPI.py
@@ -3,13 +3,16 @@ import argparse
 from mpi4py import MPI
 
 from pysingfel.radiationDamage import *
-from pysingfel.util import *
+import pysingfel.util as pu
 
 
 def main():
     """
     Main function to implement the master-slave model for parallel execution.
+
+    :return:
     """
+
     # Delete the first argument from the command line, which is the file name.
     del sys.argv[0]
     # Parse the input command line argumment to get parameters for simulation.
@@ -33,24 +36,31 @@ def main():
 
 
 def master_diffract(comm, parameters):
-    pmiStartID = int(parameters['pmiStartID'])
-    pmiEndID = int(parameters['pmiEndID'])
-    numDP = int(parameters['numDP'])
+    """
+    Master node. Get the diffraction patterns with mpi
+
+    :param comm: MPI comm
+    :param parameters:
+    :return:
+    """
+    pmi_start_id = int(parameters['pmiStartID'])
+    pmi_end_id = int(parameters['pmiEndID'])
+    num_dp = int(parameters['numDP'])
 
     # Number of processes
-    numProcesses = comm.Get_size()
-    ntasks = (pmiEndID - pmiStartID + 1) * numDP
+    num_process = comm.Get_size()
+    ntasks = (pmi_end_id - pmi_start_id + 1) * num_dp
 
-    if numProcesses == 1:
-        rotationAxis = parameters['rotationAxis']
-        uniformRotation = parameters['uniformRotation']
-        myQuaternions = generateRotations(uniformRotation, rotationAxis, ntasks)
-        outputName = parameters['outputDir'] + '/diffr_out_0000001.h5'
-        if os.path.exists(outputName):
-            os.remove(outputName)
-        prepH5(outputName)
+    if num_process == 1:
+        rotation_axis = parameters['rotationAxis']
+        uniform_rotation = parameters['uniformRotation']
+        my_quaternion = generate_rotations(uniform_rotation, rotation_axis, ntasks)
+        output_name = parameters['outputDir'] + '/diffr_out_0000001.h5'
+        if os.path.exists(output_name):
+            os.remove(output_name)
+        pu.prep_h5(output_name)
         for ntask in range(ntasks):
-            MakeOneDiffr(myQuaternions, ntask, parameters, outputName)
+            make_one_diffr(my_quaternion, ntask, parameters, output_name)
     else:
         for ntask in range(ntasks):
             status = MPI.Status()
@@ -62,24 +72,31 @@ def master_diffract(comm, parameters):
             comm.send(ntask, dest=rnk)
 
     # Final send: stop all processes from waiting for tasks
-    for process in range(1, numProcesses):
+    for process in range(1, num_process):
         comm.send(-1, dest=process)
 
 
 def slave_diffract(comm, parameters):
-    pmiStartID = int(parameters['pmiStartID'])
-    pmiEndID = int(parameters['pmiEndID'])
-    numDP = int(parameters['numDP'])
-    ntasks = (pmiEndID - pmiStartID + 1) * numDP
-    rotationAxis = parameters['rotationAxis']
-    uniformRotation = parameters['uniformRotation']
-    myQuaternions = generateRotations(uniformRotation, rotationAxis, ntasks)
+    """
+    Slave node. Get the diffraction patterns with mpi
+
+    :param comm: MPI comm
+    :param parameters:
+    :return:
+    """
+    pmi_start_id = int(parameters['pmiStartID'])
+    pmi_end_id = int(parameters['pmiEndID'])
+    num_dp = int(parameters['numDP'])
+    ntasks = (pmi_end_id - pmi_start_id + 1) * num_dp
+    rotation_axis = parameters['rotationAxis']
+    uniform_rotation = parameters['uniformRotation']
+    my_quaternion = generate_rotations(uniform_rotation, rotation_axis, ntasks)
 
     # Setup output file
-    outputName = parameters['outputDir'] + '/diffr_out_' + '{0:07}'.format(comm.Get_rank()) + '.h5'
-    if os.path.exists(outputName):
-        os.remove(outputName)
-    prepH5(outputName)
+    output_name = parameters['outputDir'] + '/diffr_out_' + '{0:07}'.format(comm.Get_rank()) + '.h5'
+    if os.path.exists(output_name):
+        os.remove(output_name)
+    pu.prep_h5(output_name)
 
     # Init a local counter
     counter = 0
@@ -91,7 +108,7 @@ def slave_diffract(comm, parameters):
         if counter < 0:
             # end of simulation
             return None
-        MakeOneDiffr(myQuaternions, counter, parameters, outputName)
+        make_one_diffr(my_quaternion, counter, parameters, output_name)
         # Show master we're ready for another task
         comm.send(counter, dest=0, tag=1)
 
@@ -99,23 +116,10 @@ def slave_diffract(comm, parameters):
 def parse_input(args):
     """
     Parse the input command arguments and return a dict containing all simulation parameters.
-    """
 
-    def ParseBoolean(b):
-        # Handle different possible Boolean types.
-        if b is None:
-            return b
-        if b == False or b == True:
-            return b
-        b = b.strip()
-        if len(b) < 1:
-            raise ValueError('Cannot parse empty string into boolean.')
-        b = b[0].lower()
-        if b == 't' or b == 'y' or b == '1':
-            return True
-        if b == 'f' or b == 'n' or b == '0':
-            return False
-        raise ValueError('Cannot parse string into boolean.')
+    :param args:
+    :return:
+    """
 
     # Instantiate the parser
     parser = argparse.ArgumentParser()
@@ -125,11 +129,11 @@ def parse_input(args):
     parser.add_argument('--geomFile', help='Geometry file defining diffraction geometry')
     parser.add_argument('--configFile', help='Absolute path to the config file')
     parser.add_argument('--rotationAxis', default='xyz', help='Euler rotation convention')
-    parser.add_argument('--uniformRotation', type=ParseBoolean,
+    parser.add_argument('--uniformRotation', type=parse_boolean,
                         help='If 1, rotates the sample uniformly in SO(3),\
                                 if 0 random orientation in SO(3),\
                                 if None (omitted): no orientation.')
-    parser.add_argument('--calculateCompton', type=ParseBoolean, default=False,
+    parser.add_argument('--calculateCompton', type=parse_boolean, default=False,
                         help='If 1, includes Compton scattering in the diffraction pattern')
     parser.add_argument('--sliceInterval', type=int, help='Calculates photon field at every slice interval')
     parser.add_argument('--numSlices', type=int,
@@ -141,6 +145,28 @@ def parse_input(args):
 
     # convert argparse to dict
     return vars(parser.parse_args(args))
+
+
+def parse_boolean(b):
+    """
+    Handle different possible Boolean types.
+
+    :param b:
+    :return:
+    """
+    if b is None:
+        return b
+    if b is False or b is True:
+        return b
+    b = b.strip()
+    if len(b) < 1:
+        raise ValueError('Cannot parse empty string into boolean.')
+    b = b[0].lower()
+    if b == 't' or b == 'y' or b == '1':
+        return True
+    if b == 'f' or b == 'n' or b == '0':
+        return False
+    raise ValueError('Cannot parse string into boolean.')
 
 
 if __name__ == '__main__':
