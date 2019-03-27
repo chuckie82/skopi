a1dff70547e697e2e4bf77e011d10476a94b7ff7
Marc Griesemer <marcgri@slac.stanford.edu>
2019-03-27 14:52:53 -0700

    simpify sends, bcast dict, and parameters

--- example3D_MPI_HDF5.py
+++ example3D_MPI_HDF5.py
@@ -5,17 +5,17 @@ import pysingfel as ps
 import pysingfel.gpu as pg
 from mpi4py import MPI
 import numpy as np
-
+import argparse
 
 def main():
     
-    number = 100
-    orient = 'uniform'
-
+    del sys.argv[0]
+    params = parse_input_arguments(sys.argv)
     
-    #params = parse_input_arguements(sys.argv)
-    params = None
-
+    
+    orient = int(params['UniformOrientation'])
+    number = int(params['numSlices'])  
+ 
     # Initialize MPI
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
@@ -24,44 +24,38 @@ def main():
     print("====================================================================")
     print("Running %d parallel MPI processes, rank=%d" % (comm.size,rank))
 
-
+ 
     if rank==0:
        t_start = MPI.Wtime()
+         
+       orientations = np.zeros((number,4))
+       particle = ps.Particle()
+       det = None
+    else:
+       det = None
+       data = None
+    if rank==0:
 
-    orientations = np.zeros((number,4))
-    particle = ps.Particle()
-    
-    if rank == 0:
-       if orient=='uniform':
+       if orient== 1:
           orientations = ps.geometry.get_uniform_quat(num_pts=number)
-       elif orient== 'random':
+       elif orient== 0:
           orientations = ps.geometry.get_random_quat(num_pts=number)
        else:
           orientations = None
 
-       #particle = ps.Particle()
        print("Reading PDB file...")
        particle.read_pdb('examples/input/3iyf.pdb',ff='WK')
        # reading beam and detector files
        beam= ps.Beam('examples/input/exp_chuck.beam')
        det = ps.PnccdDetector(geom='examples/lcls/amo86615/PNCCD::CalibV1/Camp.0:pnCCD.1/geometry/0-end.data',beam=beam)
-    else:
-        #beam= None
-        particle = None
-        det = None 
-        ori= None
-        print("Broadcasting input to processes...")
-   
-    p = comm.bcast(particle, root=0)
-    #b= comm.bcast(beam,root=0)
-    d = comm.bcast(det,root=0)
-    ori = comm.bcast(orientations,root=0) 
-
-
+       print("Broadcasting input to processes...")
+    
+       data = {'particle': particle, 'orientations': orientations,'detector': det}
+    dct = comm.bcast(data,root=0)
     
     if rank==0:
-
-       pattern_shape = d.pedestal.shape  
+       
+       pattern_shape = det.pedestal.shape  
        print("Creating HDF5 file and datasets...")
        ff = h5.File('saveHDF5_parallel.h5','w')
        dat1 = ff.create_dataset('img',shape=(number,pattern_shape[0],pattern_shape[1],pattern_shape[2]),dtype=np.int32,compression="gzip", compression_opts=4)
@@ -72,31 +66,24 @@ def main():
     
        c = 0
           
-    
-       while True:
+       while c < number:
            
            status1 = MPI.Status()
-           task = comm.recv(source=MPI.ANY_SOURCE,tag=2,status=status1)
-           i = status1.Get_source()
-           #print("Rank 0: Received image %d from rank %d", (task,i))
-           # put processed image and orientation into dataset
-           if task != -1:
-              img = comm.recv(source=MPI.ANY_SOURCE,tag=1)
-              print("Rank 0: Received image %d from rank %d" % (task,i)) 
-              dat1[task,:,:,:] = img
-              dat2[task,:] = orientations[task,:]
-              dat3[task,:] = task
-           else:
-              c += 1
-              print("Kill signal %d" % c)
-           if c== sz-1:
-              break
-           #print("Task %d writing image and orientation to HDF5 dataset" % task)
+           result = comm.recv(source=MPI.ANY_SOURCE,status=status1)
            
+           i = status1.Get_source()
+           task = result[1]
+
+           print("Rank 0: Received image %d from rank %d" % (task,i)) 
+           dat1[task,:,:,:] = result[0]
+           dat2[task,:] = orientations[task,:]
+           dat3[task,:] = task
 
     else: # slave
-        
+        ori = dct['orientations']
         # initial calculations
+        d = dct['detector']
+        p = dct['particle']
         slices_num = ori.shape[0]
         pattern_shape = d.pedestal.shape
         pixel_momentum = d.pixel_position_reciprocal
@@ -106,7 +93,6 @@ def main():
         volume = pg.diffraction.calculate_diffraction_pattern_gpu(mesh, p,return_type='intensity')
         
         
-
         for counter in range((rank-1),number,sz-1):
            
            # transform quaternion (set of orientations) into 3D rotation  
@@ -115,13 +101,7 @@ def main():
            oneSlice = slave_calc_pattern(rot3d=rotmat,pixel_momentum=pixel_momentum,pattern_shape=pattern_shape,volume=volume,voxel_length=voxel_length)
 
            print("Sending slice %d from rank %d" % (counter,rank))
-           comm.send(counter,dest=0,tag=2)
-           comm.send(sliceOne,dest=0,tag=1)
-           print("Sending counter from rank %d" % rank)
-           #comm.send(counter,dest=0, tag=2)
-        counter = -1
-        comm.send(counter,tag=2,dest=0)
-    #comm.Barrier()
+           comm.send((sliceOne,counter),dest=0)
     if rank==0:
        t_end = MPI.Wtime()
        print("Finishing constructing %d patterns in %f seconds" % (number,t_end - t_start ))
@@ -150,7 +130,7 @@ def parse_input_arguments(args):
     parse.add_argument('--detectorFile', help='f')
     parse.add_argument('--beamFile', help='Beam file defining X-ray beam')
     parse.add_argument('--numSlices',type=int, help='Number of slices/diffraction patterns')
-    parse.add_argument('--rotation', type=int, help='Uniform, random, or no orientation')
+    parse.add_argument('--UniformOrientation', type=int, help='Uniform (1), random (0), or no orientation')
     # convert argparse to dict
     return vars(parse.parse_args(args))
 
