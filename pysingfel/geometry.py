Correct quaternion - rotmat for consistency.
Function quaternion2rot3d was turning clockwise instead of counter-clockwise.
Function rotmat_to_quaternion was expecting (qi, qj, qk, qr) instead of (qr, qi, qj, qk) for quaternion input.

pysingfel/geometry.py
@@ -554,11 +554,11 @@ def angle_axis_to_rot3d(axis, theta):
 def euler_to_rot3d(psi, theta, phi):
     """
     Convert rotation with euler angle (psi, theta, phi) to a rotation matrix in 3D.
-    
-    :param psi: 
-    :param theta: 
-    :param phi: 
-    :return: 
+
+    :param psi:
+    :param theta:
+    :param phi:
+    :return:
     """
 
     rphi = np.array([[np.cos(phi), np.sin(phi), 0],
@@ -657,9 +657,13 @@ def quaternion_to_angle_axis(quaternion):
     return theta, axis
 
 
+@jit
 def rotmat_to_quaternion(rotmat):
     """
-    Convert the rotation matrix to a quaternion
+    Convert the rotation matrix to a quaternion.
+
+    This function is adopted form
+    http://www.euclideanspace.com/maths/geometry/rotations/conversions/matrixToQuaternion/
 
     :param rotmat:
     :return:
@@ -673,33 +677,33 @@ def rotmat_to_quaternion(rotmat):
     r20 = rotmat[2,0]
     r21 = rotmat[2,1]
     r22 = rotmat[2,2]
-    
+
     tr = r00 + r11 + r22
-    quat = np.zeros((4,1))
+    quat = np.zeros((4,))
     if tr > 0:
         S = np.sqrt(tr+1.0) * 2.   # S=4*qw
-        quat[0] = (r21 - r12) / S
-        quat[1] = (r02 - r20) / S
-        quat[2] = (r10 - r01) / S
-        quat[3] = 0.25 * S
+        quat[0] = 0.25 * S
+        quat[1] = (r21 - r12) / S
+        quat[2] = (r02 - r20) / S
+        quat[3] = (r10 - r01) / S
     elif (r00 > r11) and (r00 > r22):
         S = np.sqrt(1.0 + r00 - r11 - r22) * 2. # S=4*qx
-        quat[0] = 0.25 * S
-        quat[1] = (r01 + r10) / S
-        quat[2] = (r02 + r20) / S
-        quat[3] = (r21 - r12) / S
+        quat[0] = (r21 - r12) / S
+        quat[1] = 0.25 * S
+        quat[2] = (r01 + r10) / S
+        quat[3] = (r02 + r20) / S
     elif r11 > r22:
         S = np.sqrt(1.0 + r11 - r00 - r22) * 2. # S=4*qy
-        quat[0] = (r01 + r10) / S
-        quat[1] = 0.25 * S
-        quat[2] = (r12 + r21) / S
-        quat[3] = (r02 - r20) / S
+        quat[0] = (r02 - r20) / S
+        quat[1] = (r01 + r10) / S
+        quat[2] = 0.25 * S
+        quat[3] = (r12 + r21) / S
     else:
         S = np.sqrt(1.0 + r22 - r00 - r11) * 2. # S=4*qz
-        quat[0] = (r02 + r20) / S
-        quat[1] = (r12 + r21) / S
-        quat[2] = 0.25 * S
-        quat[3] = (r10 - r01) / S
+        quat[0] = (r10 - r01) / S
+        quat[1] = (r02 + r20) / S
+        quat[2] = (r12 + r21) / S
+        quat[3] = 0.25 * S
 
     return quat
 
@@ -709,34 +713,21 @@ def quaternion2rot3d(quat):
     """
     Convert the quaternion to rotation matrix.
 
-    This function is adopted from
-    https://github.com/duaneloh/Dragonfly/blob/master/src/interp.c
-
     :param quat: The quaterion.
     :return: The 3D rotation matrix
     """
+    q = np.outer(quat, quat)
 
-    q01 = quat[0] * quat[1]
-    q02 = quat[0] * quat[2]
-    q03 = quat[0] * quat[3]
-    q11 = quat[1] * quat[1]
-    q12 = quat[1] * quat[2]
-    q13 = quat[1] * quat[3]
-    q22 = quat[2] * quat[2]
-    q23 = quat[2] * quat[3]
-    q33 = quat[3] * quat[3]
-
-    # Obtain the rotation matrix
     rotation = np.zeros((3, 3))
-    rotation[0, 0] = (1. - 2. * (q22 + q33))
-    rotation[0, 1] = 2. * (q12 + q03)
-    rotation[0, 2] = 2. * (q13 - q02)
-    rotation[1, 0] = 2. * (q12 - q03)
-    rotation[1, 1] = (1. - 2. * (q11 + q33))
-    rotation[1, 2] = 2. * (q01 + q23)
-    rotation[2, 0] = 2. * (q02 + q13)
-    rotation[2, 1] = 2. * (q23 - q01)
-    rotation[2, 2] = (1. - 2. * (q11 + q22))
+    rotation[0, 0] = (1. - 2. * (q[2, 2] + q[3, 3]))
+    rotation[0, 1] = 2. * (q[1, 2] - q[0, 3])
+    rotation[0, 2] = 2. * (q[1, 3] + q[0, 2])
+    rotation[1, 0] = 2. * (q[1, 2] + q[0, 3])
+    rotation[1, 1] = (1. - 2. * (q[1, 1] + q[3, 3]))
+    rotation[1, 2] = 2. * (q[2, 3] - q[0, 1])
+    rotation[2, 0] = 2. * (q[1, 3] - q[0, 2])
+    rotation[2, 1] = 2. * (q[2, 3] + q[0, 1])
+    rotation[2, 2] = (1. - 2. * (q[1, 1] + q[2, 2]))
 
     return rotation
 
@@ -747,7 +738,7 @@ def points_on_1sphere(num_pts, rotation_axis):
     Given number of points and axis of rotation, distribute
     evenly on the surface of a 1-sphere (circle).
 
-    :param num_pts: Number of points 
+    :param num_pts: Number of points
     :param rotation_axis: Rotation axis.
     :return: Quaternion list of shape [number of quaternion, 4]
     """
@@ -770,7 +761,7 @@ def points_on_2sphere(num_pts):
     """
     Given number of points, distribute evenly on hyper surface of a 4-sphere.
 
-    :param num_pts: Number of points 
+    :param num_pts: Number of points
     :return: Quaternion list of shape [number of quaternion, 4]
     """
 