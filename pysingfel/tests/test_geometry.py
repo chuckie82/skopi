6513effa8a08f7d5f005b3fa25b6516dc176c51d
Antoine Dujardin <dujardin@slac.stanford.edu>
2019-05-20 17:16:02 -0700

    Comment and clarify tests

--- pysingfel/tests/test_geometry.py
+++ pysingfel/tests/test_geometry.py
@@ -7,15 +7,18 @@ Rx90 = np.array([
     [1., 0., 0.],
     [0., 0., -1.],
     [0., 1., 0.]])
-
-
 Ry90 = np.array([
     [0., 0., 1.],
     [0., 1., 0.],
     [-1., 0., 0.]])
 
 
+quatx90 = np.array([1., 1., 0., 0.]) / np.sqrt(2)
+quaty90 = np.array([1., 0., 1., 0.]) / np.sqrt(2)
+
+
 def test_angle_axis_to_rot3d_x():
+    """Test angle_axis_to_rot3d for 90° rotations along x."""
     axis = np.array([1., 0., 0.])
     theta = np.pi/2
     rot90 = geometry.angle_axis_to_rot3d(axis, theta)
@@ -23,6 +26,7 @@ def test_angle_axis_to_rot3d_x():
 
 
 def test_angle_axis_to_rot3d_y():
+    """Test angle_axis_to_rot3d for 90° rotations along y."""
     axis = np.array([0., 1., 0.])
     theta = np.pi/2
     rot90 = geometry.angle_axis_to_rot3d(axis, theta)
@@ -30,6 +34,11 @@ def test_angle_axis_to_rot3d_y():
 
 
 def test_angle_axis_to_rot3d_invariant():
+    """Test the invariance of angle_axis_to_rot3d.
+
+    Test the invariance property of the rotation axis on randomly
+    selected axes.
+    """
     n = 1000
     orientations = geometry.get_random_quat(n)
     thetas = np.random.rand(n) * 2 * np.pi
@@ -42,18 +51,23 @@ def test_angle_axis_to_rot3d_invariant():
 
 
 def test_quaternion2rot3d_x():
-    quat = np.array([1., 1., 0., 0.]) / np.sqrt(2)
-    rot90 = geometry.quaternion2rot3d(quat)
+    """Test quaternion2rot3d for 90° rotations along x."""
+    rot90 = geometry.quaternion2rot3d(quatx90)
     assert np.allclose(rot90, Rx90)
 
 
 def test_quaternion2rot3d_y():
-    quat = np.array([1., 0., 1., 0.]) / np.sqrt(2)
-    rot90 = geometry.quaternion2rot3d(quat)
+    """Test quaternion2rot3d for 90° rotations along y."""
+    rot90 = geometry.quaternion2rot3d(quaty90)
     assert np.allclose(rot90, Ry90)
 
 
 def test_quaternion2rot3d_invariant():
+    """Test the invariance of quaternion2rot3d.
+
+    Test the invariance property of the rotation axis on randomly
+    selected axes.
+    """
     n = 1000
     orientations = geometry.get_random_quat(n)
     for i in range(n):
@@ -64,26 +78,25 @@ def test_quaternion2rot3d_invariant():
 
 
 def test_rotmat_to_quaternion_x():
+    """Test rotmat_to_quaternion for 90° rotations along x."""
     quat = geometry.rotmat_to_quaternion(Rx90)
-    assert np.allclose(quat,
-        np.array([1., 1., 0., 0.])/np.sqrt(2))
+    assert np.allclose(quat, quatx90)
 
 
 def test_rotmat_to_quaternion_y():
+    """Test rotmat_to_quaternion for 90° rotations along y."""
     quat = geometry.rotmat_to_quaternion(Ry90)
-    assert np.allclose(quat,
-        np.array([1., 0., 1., 0.])/np.sqrt(2))
+    assert np.allclose(quat, quaty90)
 
 
 def test_quat2rot2quat():
-    #n = 1000
-    #orientations = geometry.get_random_quat(n)
-    sqrt22 = np.sqrt(2)/2
-    n = 1
-    orientations = np.array([
-        [sqrt22, sqrt22, 0., 0.]])
+    """Test quaternion2rot3d and rotmat_to_quaternion consistency."""
+    n = 1000
+    orientations = geometry.get_random_quat(n)
     for i in range(n):
         orientation = orientations[i]
         rot = geometry.quaternion2rot3d(orientation)
         quat = geometry.rotmat_to_quaternion(rot)
-        assert np.allclose(orientation, quat)
+        assert np.allclose(orientation, quat) \
+            or np.allclose(orientation, -quat)
+        # quaternions doulbe-cover 3D rotations
