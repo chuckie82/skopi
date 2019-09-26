f9ab8993db079599eeaf9165ab9fa003161bf7f1
Antoine Dujardin <dujardin@slac.stanford.edu>
2019-09-26 13:57:43 -0700

    Oops.

--- pysingfel/detector/pnccd.py
+++ pysingfel/detector/pnccd.py
@@ -150,7 +150,7 @@ class PnccdDetector(DetectorBase):
 
     @property
     def pedestals(self):
-        if six.PY3 and self._pedestals is not None:
+        if six.PY3 and self._pedestals is None:
             self._pedestals = calib_constants(
                 self.det, exp=self.exp, ctype="pedestals",
                 run=self.run_num)[0]
@@ -158,7 +158,7 @@ class PnccdDetector(DetectorBase):
 
     @property
     def pixel_rms(self):
-        if six.PY3 and self._pixel_rms is not None:
+        if six.PY3 and self._pixel_rms is None:
             self._pixel_rms = calib_constants(
                 self.det, exp=self.exp, ctype="pixel_rms",
                 run=self.run_num)[0]
@@ -166,7 +166,7 @@ class PnccdDetector(DetectorBase):
 
     @property
     def pixel_mask(self):
-        if six.PY3 and self._pixel_mask is not None:
+        if six.PY3 and self._pixel_mask is None:
             self._pixel_mask = calib_constants(
                 self.det, exp=self.exp, ctype="pixel_mask",
                 run=self.run_num)[0]
@@ -174,7 +174,7 @@ class PnccdDetector(DetectorBase):
 
     @property
     def pixel_bkgd(self):
-        if six.PY3 and self._pixel_bkgd is not None:
+        if six.PY3 and self._pixel_bkgd is None:
             self._pixel_bkgd = calib_constants(
                 self.det, exp=self.exp, ctype="pixel_bkgd",
                 run=self.run_num)[0]
@@ -182,7 +182,7 @@ class PnccdDetector(DetectorBase):
 
     @property
     def pixel_status(self):
-        if six.PY3 and self._pixel_status is not None:
+        if six.PY3 and self._pixel_status is None:
             self._pixel_status = calib_constants(
                 self.det, exp=self.exp, ctype="pixel_status",
                 run=self.run_num)[0]
@@ -190,7 +190,7 @@ class PnccdDetector(DetectorBase):
 
     @property
     def pixel_gain(self):
-        if six.PY3 and self._pixel_gain is not None:
+        if six.PY3 and self._pixel_gain is None:
             self._pixel_gain = calib_constants(
                 self.det, exp=self.exp, ctype="pixel_gain",
                 run=self.run_num)[0]
