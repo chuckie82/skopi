45fe94a380c1c4a2e8b178d3cc5cbd53b3918343
Iris Chang <iris@slac.stanford.edu>
2020-02-20 14:45:56 -0800

    add remove polarization

--- examples/scripts/ExampleMultipleChaperones.py
+++ examples/scripts/ExampleMultipleChaperones.py
@@ -22,7 +22,7 @@ particleCl.read_pdb(os.path.join(pwd,'../input/pdb/3j03.pdb'), ff='WK')
 # Load beam
 beam = ps.Beam(os.path.join(pwd,'../input/beam/amo86615.beam'))
 
-geom = os.path.join(pwd,'../input/lcls/amo86615/PNCCD::CalibV1/Camp.0:pnCCD.1/geometry/0-end.data')
+geom = os.path.join(pwd,'../input/geometry/0-end.data')
 
 # Load and initialize the detector
 det = ps.PnccdDetector(geom=geom, beam = beam)
