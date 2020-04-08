46e744918d5ade1253069b403a10426ff9f0a5cb
Antoine Dujardin <dujardin@slac.stanford.edu>
2020-04-07 20:48:47 -0700

    Split exp.generate_image and generate_image_stack for assembled/unassembled detector.

--- pysingfel/experiment/base.py
+++ pysingfel/experiment/base.py
@@ -25,6 +25,10 @@ class Experiment(object):
                 pg.calculate_diffraction_pattern_gpu(mesh, particle, return_type='complex_field'))
 
     def generate_image(self):
+        img_stack = self.generate_image_stack()
+        return self.det.assemble_image_stack(img_stack)
+
+    def generate_image_stack(self):
         beam_spectrum = self.beam.generate_new_state()
         sample_state = self.generate_new_sample_state()
         img_stack = 0.
@@ -41,7 +45,7 @@ class Experiment(object):
             raw_img = recidet.add_correction_and_quantization(group_pattern)
             img_stack += raw_img
 
-        return self.det.assemble_image_stack(img_stack)
+        return img_stack
 
     def _generate_group_complex_pattern(self, recidet, i, particle_group):
         positions, orientations = particle_group
