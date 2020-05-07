04075abc3ed55207224b858a6a80e4703ef4bfd9
Iris Chang <iris@slac.stanford.edu>
2020-05-07 15:53:42 -0700

    Fix comments

--- examples/scripts/SPI_MPI_psana.sh
+++ examples/scripts/SPI_MPI_psana.sh
@@ -13,4 +13,5 @@ PYSINGFEL_DIR=os.environ["PYSINGFEL_DIR"]
 
 # source the psana2 environment
 
-srun python SPI_MPI.py --pdb=$PYSINGFEL_DIR/input/pdb/3iyf.pdb --geom=$PYSINGFEL_DIR/input/lcls/amo86615/PNCCD::CalibV1/Camp.0:pnCCD.1/geometry/0-end.data --beam=$PYSINGFEL_DIR/input/beam/amo86615.beam --numPatterns=1000 --outDir=$PYSINGFEL_DIR/output
+cd $PYSINGFEL/examples/scripts
+srun python SPI_MPI.py --pdb=$PYSINGFEL_DIR/examples/input/pdb/3iyf.pdb --geom=$PYSINGFEL_DIR/examples/input/lcls/amo86615/PNCCD::CalibV1/Camp.0:pnCCD.1/geometry/0-end.data --beam=$PYSINGFEL_DIR/examples/input/beam/amo86615.beam --numPatterns=1000 --outDir=$PYSINGFEL_DIR/examples/output
