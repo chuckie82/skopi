adf6614f1d31cbe08f266aab1f158ca243ff4dc5
Iris Chang <iris@slac.stanford.edu>
2020-05-07 15:39:19 -0700

    Fix comments

--- examples/scripts/SPI_MPI_psana.sh
+++ examples/scripts/SPI_MPI_psana.sh
@@ -6,11 +6,11 @@
 #SBATCH --gres=gpu:1080ti:1
 #SBATCH --ntasks=8
 #SBATCH --tasks-per-node=4
-#SBATCH --mail-user=iris@slac.stanford.edu
+#SBATCH --mail-user=user@slac.stanford.edu
 #SBATCH --mail-type=ALL
 
+PYSINGFEL_DIR=os.environ["PYSINGFEL_DIR"]
 
-CWD=/reg/neh/home/iris/Software/pysingfel/examples
-source ~iris/lcls2/setup_env.sh
-export PYTHONPATH=/reg/neh/home/iris/Software/pysingfel:/reg/neh/home/iris/lcls2/install/lib/python3.7/site-packages:$PYTHONPATH
-srun python SPI_MPI.py --pdb=$CWD/input/pdb/3iyf.pdb --geom=$CWD/input/lcls/amo86615/PNCCD::CalibV1/Camp.0:pnCCD.1/geometry/0-end.data --beam=$CWD/input/beam/amo86615.beam --numPatterns=1000 --outDir=$CWD/output
+# source the psana2 environment
+
+srun python SPI_MPI.py --pdb=$PYSINGFEL_DIR/input/pdb/3iyf.pdb --geom=$PYSINGFEL_DIR/input/lcls/amo86615/PNCCD::CalibV1/Camp.0:pnCCD.1/geometry/0-end.data --beam=$PYSINGFEL_DIR/input/beam/amo86615.beam --numPatterns=1000 --outDir=$PYSINGFEL_DIR/output
