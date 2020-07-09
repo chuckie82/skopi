ec3c7e27ac72d052151eaeeb273602fc8bfe25de
Iris Chang <iris@slac.stanford.edu>
2020-07-09 11:42:32 -0700

    JungfrauDetector initialization

--- pysingfel/detector/jungfrau.py
+++ pysingfel/detector/jungfrau.py
@@ -14,34 +14,15 @@ class JungfrauDetector(AutoRangingDetector):
                          "fixedHigh", "fixedMedium", "fixedLow"] ## fixed
 
         self.nBits = 14
-        self.maxEnergy = 10000*8 ## keV
-        self.nRanges = 2
-        if self.cameraConfig in ["mediumLow", "fixedMedium", "fixedLow"]:
-            self.switchPoints = [300*8, self.maxEnergy*666] ## keV
-            if self.cameraConfig == "fixedMedium":
-                tmp = self.switchPoints[0]
-                self.switchPoints[0] = self.maxEnergy*666
-                self.maxEnergy = tmp
-            if self.cameraConfig == "fixedLow":
-                self.switchPoints[0] = -666
-            self.switchPointVariations = [4.*8, 0.] ## keV, placeholder
-            self.switchPointWidths = [4.*8, 0.] ## keV, placeholder
-            self.offsets = [0, 2100] ## ADU; placeholder
-            self.gains = [4.1, 0.1] ## ADU/keV
-            self.gainErrors = [0.01, 0.015] ## dimensionless, placeholder
-            self.nonLinearity = [0.01/2400, 0.017/self.maxEnergy] ## 1/keV, placeholder
-        else:
-            self.switchPoints = [100*8, self.maxEnergy*666] ## keV
-            if self.cameraConfig == "fixedHigh":
-                tmp = self.switchPoints[0]
-                self.switchPoints[0] = self.maxEnergy*666
-                self.maxEnergy = tmp
-            self.switchPointVariations = [2.*8, 0.] ## keV, placeholder
-            self.switchPointWidths = [2.*8, 0.] ## keV, placeholder
-            self.offsets = [0, 1100] ## ADU; placeholder
-            self.gains = [12.5, 0.1] ## ADU/keV
-            self.gainErrors = [0.01, 0.015] ## dimensionless, placeholder
-            self.nonLinearity = [0.01/800, 0.017/self.maxEnergy] ## 1/keV, placeholder
+        self.maxEnergy = 15000*8 ##keV
+        self.nRanges = 3
+        self.switchPoints = [40*8, 1500*8, self.maxEnergy*666] ## keV
+        self.switchPointVariations = [1.*8, 10.*8, 0.] ## keV, placeholder
+        self.switchPointWidths = [1.*8, 10.*8, 0.] ## keV, placeholder
+        self.offsets = [0, 2100, 1900] ## ADU; placeholder
+        self.gains = [40., 1., 0.1] ## ADU/keV
+        self.gainErrors = [0.01, 0.015, 0.02] ## dimensionless, placeholder
+        self.nonLinearity = [0.01/320, 0.015/12000, 0.017/self.maxEnergy] ## 1/keV, placeholder
 
         self.minE = [0.] + self.switchPoints[0:self.nRanges-1] ## keV
         ## this is for adding nonlinearity
