2e30492d2eba81bf9992745c8c8fa9a88544bcc4
chuckie82 <chuckie82@gmail.com>
2022-02-23 10:47:14 -0800

    prevent import of lcls detectors unless psana is available

--- skopi/detector/__init__.py
+++ skopi/detector/__init__.py
@@ -3,8 +3,22 @@ from .plain import PlainDetector
 from .user_defined import UserDefinedDetector
 from .simple_square import SimpleSquareDetector
 
-from .lcls import LCLSDetector
-from .pnccd import PnccdDetector
-from .cspad import CsPadDetector
-from .epix10k import Epix10kDetector
-from .jungfrau import JungfrauDetector
+global psana_version
+try:
+    from PSCalib.GeometryAccess import GeometryAccess
+    psana_version=1
+except Exception:
+    try:
+        from psana.pscalib.geometry.GeometryAccess import GeometryAccess
+        psana_version=2
+    except:
+        # psana unavailable; skip all AutorangingDetector tests
+        psana_version=0
+
+psana_version = 0
+if psana_version > 0:
+    from .lcls import LCLSDetector
+    from .pnccd import PnccdDetector
+    from .cspad import CsPadDetector
+    from .epix10k import Epix10kDetector
+    from .jungfrau import JungfrauDetector
