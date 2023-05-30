5ea0211eadf9eacd3fb3759ce6bce3a772c7b5fd
monarin <monarin@gmail.com>
2023-05-30 13:46:22 -0400

    remove appear-to-be unused functions in diffraction module

--- skopi/diffraction.py
+++ skopi/diffraction.py
@@ -1,5 +1,4 @@
 import numpy as np
-from numba import jit
 from scipy.interpolate import CubicSpline
 from skopi.util import xp, asnumpy
 
@@ -71,60 +70,3 @@ def calculate_atomic_factor(particle, q_space, pixel_num):
     f_hkl = np.reshape(f_hkl, (particle.num_atom_types,) + q_space.shape)
     return xp.asarray(f_hkl)
 
-
-@jit
-def get_phase(atom_pos, q_xyz):
-    """
-    Calculate the phase of the diffraction field due to the specific atom
-    :param atom_pos: The atom position
-    :param q_xyz: The reciprocal space to calculate.
-    :return:
-    """
-    phase = 2 * np.pi * (atom_pos[0] * q_xyz[:, 0] +
-                         atom_pos[1] * q_xyz[:, 1] +
-                         atom_pos[2] * q_xyz[:, 2])
-    return np.exp(1j * phase)
-
-
-@jit
-def cal(f_hkl, atom_pos, q_xyz, xyz_ind, pixel_number):
-    """
-    Calculate the diffraction intensity field.
-
-    :param f_hkl: The form factor array
-    :param atom_pos:  The atom position array
-    :param q_xyz: The reciprocal space to calculate.
-    :param xyz_ind: The split index.
-    :param pixel_number: number of pixels.
-    :return:
-    """
-    f = np.zeros(pixel_number, dtype=np.complex128)
-    for atm in range(atom_pos.shape[0]):
-        f += get_phase(atom_pos[atm, :], q_xyz) * f_hkl[xyz_ind[atm], :]
-    return np.abs(f) ** 2
-
-
-def calculate_molecular_form_factor_square(particle, q_space, q_position):
-    """
-    Calculate the diffraction intensity field of the molecule.
-
-    :param particle: The particle object.
-    :param q_space: The reciprocal distance of the pixels.
-    :param q_position: The reciprocal position of the pixels.
-    :return:
-    """
-    shape = q_position.shape
-    pixel_number = np.prod(shape[:-1])
-    q_space_1d = np.reshape(q_space, [pixel_number, ])
-    q_position_1d = np.reshape(q_position, [pixel_number, 3])
-
-    f_hkl = calculate_atomic_factor(particle, q_space_1d, pixel_number)
-    split_index = particle.split_idx[:]
-    xyz_ind = np.zeros(split_index[-1], dtype=int)
-    for i in range(len(split_index) - 1):
-        xyz_ind[split_index[i]:split_index[i + 1]] = i
-
-    pattern_1d = cal(f_hkl, particle.atom_pos, q_position_1d, xyz_ind, pixel_number)
-    pattern = np.reshape(pattern_1d, shape[:-1])
-
-    return pattern
